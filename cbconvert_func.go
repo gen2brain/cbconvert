f795c70c3304ce792102e8a0cf9e6296ee0fbff4
Milan Nikolic <gen2brain@gmail.com>
2024-11-03 22:00:51 +0100

    Add office documents

--- cbconvert_func.go
+++ cbconvert_func.go
@@ -73,7 +73,7 @@ func isArchive(f string) bool {
 
 // isDocument checks if file is document.
 func isDocument(f string) bool {
-	var types = []string{".pdf", ".xps", ".epub", ".mobi", ".docx", ".pptx"}
+	var types = []string{".pdf", ".xps", ".epub", ".mobi", ".docx", ".pptx", ".xlsx"}
 	for _, t := range types {
 		if strings.ToLower(filepath.Ext(f)) == t {
 			return true
